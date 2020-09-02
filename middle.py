84d595ccbd1897033fe071f1abf8be2d6d73a31a
avo31 <60754683+avo31@users.noreply.github.com>
2020-09-02 13:27:18 -0500

    Add files via upload

---
+++ middle.py
@@ -0,0 +1,32 @@
+# Andrew Vo
+
+import sys
+import os
+
+fileName = input("Enter file name: ")
+
+if not os.path.isfile(fileName):
+    print("File not found.")
+    sys.exit()
+
+
+flagIn = input("Enter number of lines to skip at top and bottom (Press enter for default value of 5): ")
+
+if flagIn == "":
+    flag = 5
+
+else:
+    flag = int(flagIn)
+
+with open(fileName) as fin:
+
+    lines = fin.readlines()
+
+    if len(lines) <= 2 * flag:
+        for line in lines:
+            print(line)
+    else:
+        for x in range(flag, len(lines) - flag):
+            print(lines[x])
+
+
